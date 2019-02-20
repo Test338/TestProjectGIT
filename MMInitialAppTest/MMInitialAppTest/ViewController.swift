63ebec57f4f6e3d9cb7b150d628c65ed6aa4ffb2
Power Tools <ptmt@LE-Z5674.localdomain>
2019-02-20 09:33:47 +0100

    new project created

---
+++ MMInitialAppTest/MMInitialAppTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MMInitialAppTest
+//
+//  Created by Power Tools on 20.02.19.
+//  Copyright © 2019 Robert Bosch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
